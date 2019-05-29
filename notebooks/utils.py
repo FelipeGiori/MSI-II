685bfc905a81d28a29b10acb91d7dd366b0f6317
FelipeGiori <felipe.giori@gmail.com>
2019-05-29 00:34:58 -0300

    Kappa fleiss most frequent

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import pandas as pd
-
 def str2tupleList(s):
     return eval("[%s]" % s)[0]
+
+def str2List(s):
+    return s.split(" ")
+
+def sumOfSquares(lst):
+    tmp = 0
+    for n in lst:
+        tmp = tmp + float(n)**2
+
+    return tmp
